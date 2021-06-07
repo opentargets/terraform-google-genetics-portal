f021e0106f79acf2155325f6140d45025bd652d3
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-07 12:41:49 +0100

    input parameter, release name

--- variables.tf
+++ variables.tf
@@ -1,1 +1,7 @@
 // Open Targets Genetics Portal Deployment Input Parameters
+// --- RELEASE INFORMATION --- //
+variable "config_release_name" {
+  description = "Deployment name used for scoping of resources created. It should have a useful meaning for easy spotting of resources in the GCP Project."
+  type = string
+}
+
