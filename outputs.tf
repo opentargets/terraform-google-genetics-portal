ae8dcbb0c14ac12b0561adc817c371a19d122f90
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-07 21:44:41 +0100

    output zone availability information

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,14 @@
 output "gcp_available_regions" {
   value = data.google_compute_regions.gcp_available_regions
 }
+output "gcp_available_zones_per_region" {
+  value = local.gcp_available_zones_per_region
+}
 
 output "subnet_indexing" {
   value = local.vpc_subnet_index
 }
 
-output "subnetting_per_region" {
+output "subnetting_per_deployment_region" {
   value = local.vpc_network_region_subnet_map
 }
