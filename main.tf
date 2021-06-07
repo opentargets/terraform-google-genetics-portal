ed627f8a0754501b151d764588a1fc92738827c7
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-07 21:44:03 +0100

    specify the project ID

--- main.tf
+++ main.tf
@@ -27,11 +27,13 @@ provider "google-beta" {
 
 // Availability Regions --- //
 data "google_compute_regions" "gcp_available_regions" {
+    project = var.config_project_id
 }
 
 // Availability Zones --- //
 data "google_compute_zones" "gcp_available_zones" {
     count = length(local.gcp_available_region_names_sorted)
+    project = var.config_project_id
 
     region = local.gcp_available_region_names_sorted[count.index]
 }
