602e758cd958d2b975d2ab4acbd9df28a6506c2c
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-07 21:21:01 +0100

    networking, main subnet name

--- locals.tf
+++ locals.tf
@@ -2,6 +2,7 @@
 locals {
   // Networking --- // 
   vpc_network_name             = "${var.config_release_name}-vpc"
+  vpc_network_main_subnet_name = "main-subnet"
   vpc_subnet_index = zipmap(
     sort(data.google_compute_regions.gcp_available_regions.names),
     range(0, length(data.google_compute_regions.gcp_available_regions.names))
