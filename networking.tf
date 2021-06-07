e969e7ef972e62f05e4457dd1062ebcd20090c0c
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-07 23:31:33 +0100

    refactoring

---
+++ networking.tf
@@ -0,0 +1,6 @@
+// --- Networking related stuff --- //
+// Default Network Tier --- //
+resource "google_compute_project_default_network_tier" "default_network_tier" {
+  network_tier = "PREMIUM"
+  project      = var.config_project_id
+}
--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,4 @@
 // --- VPC --- //
-// Default Network Tier --- //
-resource "google_compute_project_default_network_tier" "default_network_tier" {
-  network_tier = "PREMIUM"
-  project      = var.config_project_id
-}
-
 // VPC with Custom Subnetting --- //
 module "vpc_network" {
   source  = "terraform-google-modules/network/google"
