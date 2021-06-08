a3c59abff4026be01b6643ee03a09f3d4f42fb26
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-08 13:55:36 +0100

    web app bundle provisioner configuration

--- modules/webapp/locals.tf
+++ modules/webapp/locals.tf
@@ -25,4 +25,15 @@ locals {
       )
     ]
   )
+  // Bundle Based provisioner --- //
+  webapp_bundle_provisioner_path_script = abspath("${path.module}/scripts/bucket_webapp_bundle_provisioner.sh")
+  webapp_bundle_provisioner_url_bundle_download = "https://github.com/${local.webapp_provisioner_repo_name}/releases/download/${var.webapp_release}/bundle.tgz"
+  webapp_bundle_provisioner_path_build = "${local.webapp_provisioner_path_working_dir}/build"
+  webapp_bundle_provisioner_file_name_devops_context_template = "config.template"
+  webapp_bundle_provisioner_file_name_devops_context_instance = "config.js"
+  webapp_bundle_provisioner_robots_active_file_name = "robots.txt"
+  webapp_bundle_provisioner_robots_profile_default = "default"
+  webapp_bundle_provisioner_robots_profile_src = var.webapp_robots_profile != local.webapp_bundle_provisioner_robots_profile_default ? "${local.webapp_bundle_provisioner_robots_active_file_name}.${var.webapp_robots_profile}" : local.webapp_bundle_provisioner_robots_active_file_name
+  webapp_bundle_provisioner_url_bucket_data_context = "gs://${var.webapp_bucket_data_context_name}/${var.webapp_bucket_data_context_release}/${var.webapp_bucket_data_context_subfolder_name}"
+  webapp_bundle_provisioner_data_context_dst_folder = "data"
 }
