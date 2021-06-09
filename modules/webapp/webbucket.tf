531984d726cb44f837dbabd6fb020b407e7d3ebd
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-09 10:58:14 +0100

    webapp bundle provisioner

--- modules/webapp/webbucket.tf
+++ modules/webapp/webbucket.tf
@@ -36,3 +36,32 @@ module "bucket_webapp" {
   enable_cors = true
   cors_origins = [ "*"]
 }
+
+// --- Web Application Content Provisioner --- //
+resource "null_resource" "webapp_provisioner" {
+  depends_on = [ module.bucket_webapp ]
+  // Redeployment will be triggered whenever the bucket name changes
+  triggers = {
+    bucket_name = module.bucket_webapp.website_bucket_name
+  }
+  provisioner "local-exec" {
+    working_dir = var.folder_tmp
+    environment = merge({
+        working_dir = local.webapp_provisioner_path_working_dir
+        url_bundle_download = local.webapp_bundle_provisioner_url_bundle_download
+        path_build = local.webapp_bundle_provisioner_path_build
+        file_name_devops_context_template = local.webapp_bundle_provisioner_file_name_devops_context_template
+        file_name_devops_context_instance = local.webapp_bundle_provisioner_file_name_devops_context_instance
+        bucket_webapp_url = "gs://${module.bucket_webapp.website_bucket_name}"
+        robots_active_file_name = local.webapp_bundle_provisioner_robots_active_file_name
+        robots_profile_src_file_name = local.webapp_bundle_provisioner_robots_profile_src
+        robots_profile_name = var.webapp_robots_profile
+        data_context_url = local.webapp_bundle_provisioner_url_bucket_data_context
+        data_context_dst_folder = local.webapp_bundle_provisioner_data_context_dst_folder
+        deployment_bundle_filename = local.webapp_deployment_bundle_filename
+      },
+      local.webapp_provisioner_deployment_context
+    )
+    command = "/bin/bash ${local.webapp_bundle_provisioner_path_script}"
+  }
+}
