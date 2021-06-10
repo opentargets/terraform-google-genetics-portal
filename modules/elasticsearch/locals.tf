3f9cbe7bd724ae542e2c7230775b2e401fdadbcb
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-10 17:50:32 +0100

    helper information

---
+++ modules/elasticsearch/locals.tf
@@ -0,0 +1,24 @@
+// Helper information
+locals {
+  // Ports
+  elastic_search_port_requests  = 9200
+  elastic_search_port_comms     = 9300
+  elastic_search_port_requests_name = "esportrequests"
+  elastic_search_port_comms_name = "esportcomms"
+  // Firewall tags
+  fw_tag_elasticsearch_requests = "elasticsearchrequests"
+  fw_tag_elasticsearch_comms = "elasticsearchcomms"
+
+  // Elastic Search instance template values
+  elastic_search_template_tags = concat(
+    var.vm_firewall_tags,
+    [
+      local.fw_tag_elasticsearch_requests,
+      local.fw_tag_elasticsearch_comms
+    ]
+  )
+  elastic_search_template_machine_type = "custom-${var.vm_elastic_search_vcpus}-${var.vm_elastic_search_mem}"
+  elastic_search_template_source_image = "${var.vm_elastic_search_image_project}/${var.vm_elastic_search_image}"
+  // Compute Zones internal parameters
+  compute_zones_n_total = length(data.google_compute_zones.gcp_available_zones.names)
+}
