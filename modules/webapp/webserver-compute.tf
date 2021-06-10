9738e4ef41399fa5002fb965124dcc82d7b847da
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-10 11:18:53 +0100

    webapp server nodes definition, entropy source

--- modules/webapp/webserver-compute.tf
+++ modules/webapp/webserver-compute.tf
@@ -3,3 +3,18 @@
 //  - Use a regional index map for fixed indexes on the different regions, to avoid the problem of pulling the first region from the list of deploymebnt regions
 //  - 
 
+// Definition of active web servers for the web application
+resource "random_string" "random_web_server_suffix" {
+  length = 8
+  lower = true
+  upper = false
+  special = false
+  keepers = {
+    webapp_bucket_name = local.bucket_name
+    deployment_bundle_filename = local.webapp_deployment_bundle_filename
+    deployment_bundle_url = local.webapp_deployment_bundle_url
+    nginx_docker_image_version = var.webserver_docker_image_version
+    startup_script = md5(file("${path.module}/scripts/webserver_vm_startup_script.sh"))
+  }
+}
+
