eda6205e8701f0cde63fd3938e8c30c9feef5900
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-10 13:17:44 +0100

    compute details on the underlying GCP infrastructure

--- modules/webapp/main.tf
+++ modules/webapp/main.tf
@@ -1,3 +1,16 @@
 // --- Open Targets Genetics Portal Web Application definition --- //
 // Author: Manuel Bernal Llinares <mbdebian@gmail.com>
 
+// --- Underlying Infrastructure Details --- //
+// Availability Regions --- //
+data "google_compute_regions" "gcp_available_regions" {
+  project = var.config_project_id
+}
+
+// Availability Zones --- //
+data "google_compute_zones" "gcp_available_zones" {
+  count   = length(local.gcp_available_region_names_sorted)
+  project = var.config_project_id
+
+  region = local.gcp_available_region_names_sorted[count.index]
+}
