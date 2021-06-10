8d04da33e2965d02228a067bacbb30283c98b5c0
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-10 11:18:31 +0100

    webapp server nodes definition, TODOs section

--- modules/webapp/webserver-compute.tf
+++ modules/webapp/webserver-compute.tf
@@ -1,1 +1,5 @@
 // --- Active Web Servers for the web application --- //
+// TODOs:
+//  - Use a regional index map for fixed indexes on the different regions, to avoid the problem of pulling the first region from the list of deploymebnt regions
+//  - 
+
