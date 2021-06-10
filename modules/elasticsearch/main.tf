95150851c4319d5ab9074bdd74ed30020226f890
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-10 17:22:34 +0100

    elastic search module, main, comments

--- modules/elasticsearch/main.tf
+++ modules/elasticsearch/main.tf
@@ -1,2 +1,6 @@
 // --- Open Targets Genetics Portal, deployment module --- //
 // Author: Manuel Bernal Llinares <mbdebian@gmail.com>
+/*
+    This module defines a Regional Elasctic Search deployment behind a ILB,
+    within the context of Open Targets Genetics Portal.
+*/
