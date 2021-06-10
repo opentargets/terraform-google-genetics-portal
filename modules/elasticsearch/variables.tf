74d471dbc4f710aaf6d3288cf8680df6ea1a1a7c
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-10 17:10:50 +0100

    elastic search module, input parameters, deployment network name

--- modules/elasticsearch/variables.tf
+++ modules/elasticsearch/variables.tf
@@ -6,3 +6,9 @@ variable "module_wide_prefix_scope" {
   default = "mdevgenes"
 }
 
+variable "network_name" {
+  description = "Name of the network where resources should be deployed, 'default'"
+  type = string
+  default = "default"
+}
+
