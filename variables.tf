3ce289fce92461e99daca2b4c9ec5dd30be96ca3
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-10 11:55:53 +0100

    input parameters for web application deployment

--- variables.tf
+++ variables.tf
@@ -27,6 +27,84 @@ variable "config_deployment_regions" {
 }
 
 // --- Platform Configuration --- //
+// TODO --- DNS configuration               --- //
+// TODO --- Elastic Search configuration    --- //
+// TODO --- Clickhouse configuration        --- //
+// TODO --- API configuration               --- //
+
+// --- Web Application configuration   --- //
+variable "config_webapp_repo_name" {
+  description = "Web Application repository name"
+  type = string
+}
+
+variable "config_webapp_release" {
+  description = "Release version of the web application to deploy"
+  type = string
+}
+
+variable "config_webapp_deployment_context_map" {
+  description = "A map with values for those parameters that need to be customized in the deployment of the web application, see module defaults as an example"
+  type = any
+}
+
+variable "config_webapp_location" {
+  description = "This input parameter defines the location of the Web Application (bucket), default 'EU'"
+  type = string
+  default = "EU"
+}
+
+variable "config_webapp_robots_profile" {
+  description = "This input parameter defines the 'robots.txt' profile to be used when deploying the web application, default 'default', which means that no changes to existing 'robots.txt' file will be made"
+  type = string
+  default = "default"
+}
+
+variable "config_webapp_bucket_name_data_assets" {
+  description = "Bucket where to find the data context for the web application"
+  type = string
+}
+
+variable "config_webapp_data_context_release" {
+  description = "Data context release for the web application"
+  type = string
+}
+
+// Web Application Web Servers --- //
+variable "config_webapp_webserver_docker_image_version" {
+  description = "NginX Docker image version to use in deployment"
+  type = string
+}
+
+variable "config_webapp_webserver_vm_vcpus" {
+  description = "CPU count, default '1'"
+  type = number
+  default = "1"
+}
+
+variable "config_webapp_webserver_vm_mem" {
+  description = "Amount of memory allocated Web Server nodes (MiB), default '3840'"
+  type = number
+  default = "3840"
+}
+
+variable "config_webapp_webserver_vm_image" {
+  description = "VM image to use for Web Server nodes, default 'cos-stable'"
+  type = string
+  default = "cos-stable"
+}
+
+variable "config_webapp_webserver_vm_image_project" {
+  description = "Project hosting the VM image, default 'cos-cloud'"
+  type = string
+  default = "cos-cloud"
+}
+
+variable "config_webapp_webserver_vm_boot_disk_size" {
+  description = "Boot disk size for Web Server nodes, default '10GB'"
+  type = string
+  default = "10GB"
+}
 
 // --- Development / Debugging Support --- //
 variable "config_enable_inspection" {
