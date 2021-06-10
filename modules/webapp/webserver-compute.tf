a64edb1b37292ec060c741db1201ad2ddd7df514
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-10 11:21:05 +0100

    webapp server nodes definition, health checks

--- modules/webapp/webserver-compute.tf
+++ modules/webapp/webserver-compute.tf
@@ -86,3 +86,16 @@ resource "google_compute_instance_template" "webserver_template" {
   }
 }
 
+// Health Check --- //
+resource "google_compute_health_check" "webserver_healthcheck" {
+  name = "${var.module_wide_prefix_scope}-webserver-healthcheck"
+  check_interval_sec = 5
+  timeout_sec = 5
+  healthy_threshold = 2
+  unhealthy_threshold = 10
+
+  tcp_health_check {
+    port = local.webapp_webserver_port
+  }
+}
+
