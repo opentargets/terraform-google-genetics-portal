adcb96b5068c8fe1d8a313889dda2d5ab4ec157a
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-11 10:47:24 +0100

    main module heading

---
+++ modules/clickhouse/main.tf
@@ -0,0 +1,6 @@
+// --- Open Targets Genetics Portal, Clickhouse deployment module --- //
+// Author: Manuel Bernal Llinares <mbdebian@gmail.com>
+/*
+    This module defines a Regional Clickhouse deployment behind a ILB,
+    within the context of Open Targets Genetics Portal.
+*/
