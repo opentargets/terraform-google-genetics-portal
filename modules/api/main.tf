fd33598e4e1956216ed439f700bb2b3ffbf77e85
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-11 13:30:48 +0100

    main infrastructure definition heading

---
+++ modules/api/main.tf
@@ -0,0 +1,5 @@
+// Open Targets Genetics Portal API deployment definition
+// Author: Manuel Bernal Llinares <mbdebian@gmail.com>
+/*
+    This module defines a multi-regional deployment of Open Target Genetics Portal API
+*/
