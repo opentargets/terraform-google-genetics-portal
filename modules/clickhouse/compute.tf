96ed5341eed946d5585c130a76c1eff3a4e04fd4
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-11 11:40:04 +0100

    module clickhouse, compute, entropy source

--- modules/clickhouse/compute.tf
+++ modules/clickhouse/compute.tf
@@ -3,3 +3,16 @@
     This module defines a regional Clickhouse deployment behind an ILB
 */
 
+// Entropy source --- //
+resource "random_string" "random_ch_vm" {
+  length = 8
+  lower = true
+  upper = false
+  special = false
+  keepers = {
+    clickhouse_template_tags = join("", sort(local.clickhouse_template_tags)),
+    clickhouse_template_machine_type = local.clickhouse_template_machine_type,
+    clickhouse_template_source_image = local.clickhouse_template_source_image
+  }
+}
+
