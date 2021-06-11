81a7e0c62bd9dcf485c4efc581fa3dd8a2231ac9
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-11 10:47:50 +0100

    get available GCP zones for the given region

--- modules/clickhouse/main.tf
+++ modules/clickhouse/main.tf
@@ -4,3 +4,10 @@
     This module defines a Regional Clickhouse deployment behind a ILB,
     within the context of Open Targets Genetics Portal.
 */
+
+// --- GCP Underlaying infrastructure details --- //
+// Access to Available compute zones in the given region --- //
+data "google_compute_zones" "gcp_available_zones" {
+  project = var.project_id
+  region = var.deployment_region
+}
