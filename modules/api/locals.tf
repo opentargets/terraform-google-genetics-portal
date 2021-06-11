a302831c013293b7c5d5b4d2c84be9e9390a1bb8
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-11 13:49:48 +0100

    helper information

--- modules/api/locals.tf
+++ modules/api/locals.tf
@@ -5,4 +5,26 @@ locals {
     local.gcp_available_region_names_sorted,
     [for region_details in data.google_compute_zones.gcp_available_zones : region_details.names]
   )
+  // API Communication Ports
+  api_port = 8080
+  api_port_name = "otgpapiport"
+  // Firewall
+  fw_tag_api_node = "otgpapinode"
+  // Load Balancer types
+  lb_type_internal = "INTERNAL"
+  lb_type_none = "NONE"
+  input_validation_load_balancer_type_allowed_values = [
+    local.lb_type_internal,
+    local.lb_type_none
+  ]
+  // API VM instance template values
+  api_template_tags = concat(
+    var.vm_firewall_tags,
+    [
+      local.fw_tag_api_node
+    ]
+  )
+  api_template_machine_type = "custom-${var.vm_api_vcpus}-${var.vm_api_mem}"
+  api_template_source_image = "${var.vm_api_image_project}/${var.vm_api_image}"
+
 }
