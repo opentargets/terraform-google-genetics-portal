68a61d19986d215c617b7ed2ec3752d4ab550cda
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-11 11:55:43 +0100

    module clickhouse, output details

---
+++ modules/clickhouse/outputs.tf
@@ -0,0 +1,33 @@
+// --- Module Output Information --- //
+output "deployment_region" {
+  value = var.deployment_region
+}
+
+output "network_name" {
+  value = var.network_name
+}
+
+output "network_subnet_name" {
+  value = var.network_subnet_name
+}
+
+output "ilb_ip_address" {
+  value = module.ilb_ch.ip_address
+}
+
+// Named Ports --- //
+output "port_clickhouse_http" {
+  value = local.clickhouse_http_req_port
+}
+
+output "port_clickhouse_http_name" {
+  value = local.clickhouse_http_req_port_name
+}
+
+output "port_clickhouse_cli" {
+  value = local.clickhouse_cli_req_port
+}
+
+output "port_clickhouse_cli_name" {
+  value = local.clickhouse_cli_req_port_name
+}
