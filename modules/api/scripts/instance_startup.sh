a9c2ae903e70ac234dd4aa3e7aefa9e6737e6d4a
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-14 12:18:05 +0100

    genetics api docker image repo

--- modules/api/scripts/instance_startup.sh
+++ modules/api/scripts/instance_startup.sh
@@ -5,4 +5,4 @@ docker run -d \
   -p ${OTP_API_PORT}:${OTP_API_PORT} \
   -e SLICK_CLICKHOUSE_URL='${SLICK_CLICKHOUSE_URL}' \
   -e ELASTICSEARCH_HOST='${ELASTICSEARCH_HOST}' \
-  quay.io/opentargets/platform-api-beta:${PLATFORM_API_VERSION}
+  quay.io/opentargets/genetics-api:${PLATFORM_API_VERSION}
