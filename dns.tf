ff0afa9fd560fde8dc6a0e0a501e4689cd751c7d
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-06-15 11:09:03 +0100

    DNS entries configuration

--- dns.tf
+++ dns.tf
@@ -1,2 +1,26 @@
 // --- Open Targets Genetics Portal DNS configuration --- //
-// TODO //
+// Entry for Open Target API GLB
+resource "google_dns_record_set" "dns_a_api_glb" {
+  // Common
+  project = var.config_dns_project_id
+  managed_zone = var.config_dns_managed_zone_name
+  type = "A"
+  ttl = 5
+
+  // Entry
+  name = local.dns_api_dns_name
+  rrdatas = [ module.glb.external_ip ]
+}
+
+// Entry for Open Targets Platform Web Application
+resource "google_dns_record_set" "dns_a_webapp_glb" {
+    count = length(local.dns_webapp_domain_names)
+    // Common
+    project = var.config_dns_project_id
+    managed_zone = var.config_dns_managed_zone_name
+    type = "A"
+    ttl = 5
+    // Entry
+    name = local.dns_webapp_domain_names[count.index]
+    rrdatas = [ module.glb.external_ip ]
+}
