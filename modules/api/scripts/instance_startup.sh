42aec2ae4fe3984ecd520c09300b562baada75e5
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-11-16 12:47:46 +0000

    use gcplogs driver in docker

--- modules/api/scripts/instance_startup.sh
+++ modules/api/scripts/instance_startup.sh
@@ -3,6 +3,7 @@
 echo "[LAUNCH] Open Targets Genetics Portal API"
 docker run -d \
   -p ${API_PORT}:${API_PORT} \
+  --log-driver=gcplogs \
   -e SLICK_CLICKHOUSE_URL='${SLICK_CLICKHOUSE_URL}' \
   -e ELASTICSEARCH_HOST='${ELASTICSEARCH_HOST}' \
   quay.io/opentargets/genetics-api:${API_VERSION}
