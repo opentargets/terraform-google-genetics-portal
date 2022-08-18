1b71e097be3b29cd9fced930294533fa787c2ba9
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-08-18 15:17:08 +0100

    Update web app provisioner to use genetics bundle.
    The FE has migrated to a monorepo. This commit points us to the
    correct repository and selects the correct bundle to deploy.

--- modules/webapp/locals.tf
+++ modules/webapp/locals.tf
@@ -1,12 +1,12 @@
 // --- Helper information --- //
 locals {
-    // Helpers --- //
+  // Helpers --- //
   gcp_available_region_names_sorted = sort(var.gcp_available_region_names)
   gcp_available_zones_per_region = zipmap(
     local.gcp_available_region_names_sorted,
     [for region_details in data.google_compute_zones.gcp_available_zones : region_details.names]
   )
-/*  gcp_regions_static_indexing = zipmap(
+  /*  gcp_regions_static_indexing = zipmap(
     local.gcp_available_region_names_sorted,
     range(0, length(local.gcp_available_region_names_sorted))
   )
@@ -18,8 +18,8 @@ locals {
     "US"
   ]
   bucket_storage_class = contains(local.multiregional_locations, var.webapp_bucket_location) ? "MULTI_REGIONAL" : "REGIONAL"
-  bucket_name = replace("${var.module_wide_prefix_scope}-${var.webapp_release}-${random_string.random_webbucket.result}", ".", "-")
-  bucket_logs_prefix = "${var.module_wide_prefix_scope}-${var.webapp_release}"
+  bucket_name          = replace("${var.module_wide_prefix_scope}-${var.webapp_release}-${random_string.random_webbucket.result}", ".", "-")
+  bucket_logs_prefix   = "${var.module_wide_prefix_scope}-${var.webapp_release}"
   // --- Web App Provisioning --- //
   // Repo name
   webapp_provisioner_repo_name = var.webapp_repo_name
@@ -29,7 +29,7 @@ locals {
   // Deployment Context
   webapp_provisioner_deployment_context = zipmap(
     keys(var.webapp_deployment_context),
-    [for key, value in var.webapp_deployment_context:
+    [for key, value in var.webapp_deployment_context :
       replace(
         value,
         "/",
@@ -38,30 +38,30 @@ locals {
     ]
   )
   // Bundle Based provisioner --- //
-  webapp_bundle_provisioner_path_script = abspath("${path.module}/scripts/bucket_webapp_bundle_provisioner.sh")
-  webapp_bundle_provisioner_url_bundle_download = "https://github.com/${local.webapp_provisioner_repo_name}/releases/download/${var.webapp_release}/bundle.tgz"
-  webapp_bundle_provisioner_path_build = "${local.webapp_provisioner_path_working_dir}/build"
+  webapp_bundle_provisioner_path_script                       = abspath("${path.module}/scripts/bucket_webapp_bundle_provisioner.sh")
+  webapp_bundle_provisioner_url_bundle_download               = "https://github.com/${local.webapp_provisioner_repo_name}/releases/download/${var.webapp_release}/bundle-genetics.tgz"
+  webapp_bundle_provisioner_path_build                        = "${local.webapp_provisioner_path_working_dir}/build"
   webapp_bundle_provisioner_file_name_devops_context_template = "config.template"
   webapp_bundle_provisioner_file_name_devops_context_instance = "config.js"
-  webapp_bundle_provisioner_robots_active_file_name = "robots.txt"
-  webapp_bundle_provisioner_robots_profile_default = "default"
-  webapp_bundle_provisioner_robots_profile_src = var.webapp_robots_profile != local.webapp_bundle_provisioner_robots_profile_default ? "${local.webapp_bundle_provisioner_robots_active_file_name}.${var.webapp_robots_profile}" : local.webapp_bundle_provisioner_robots_active_file_name
-  
+  webapp_bundle_provisioner_robots_active_file_name           = "robots.txt"
+  webapp_bundle_provisioner_robots_profile_default            = "default"
+  webapp_bundle_provisioner_robots_profile_src                = var.webapp_robots_profile != local.webapp_bundle_provisioner_robots_profile_default ? "${local.webapp_bundle_provisioner_robots_active_file_name}.${var.webapp_robots_profile}" : local.webapp_bundle_provisioner_robots_active_file_name
+
   // --- Web Servers --- //
   // Communication Ports
-  webapp_webserver_port = 8080
+  webapp_webserver_port      = 8080
   webapp_webserver_port_name = "webappserverport"
   // Firewall
   fw_tag_webserver_node = "webappservernode"
   // Web Server VM instance template values
   webapp_webserver_template_tags = concat(
     var.webserver_firewall_tags,
-    [ local.fw_tag_webserver_node ]
+    [local.fw_tag_webserver_node]
   )
   // Machine geometry
   webapp_webserver_template_machine_type = "custom-${var.webserver_vm_vcpus}-${var.webserver_vm_mem}"
   webapp_webserver_template_source_image = "${var.webserver_vm_image_project}/${var.webserver_vm_image}"
   // Web Application Deployment Bundle
   webapp_deployment_bundle_filename = "deployment_bundle.tgz"
-  webapp_deployment_bundle_url = "https://storage.googleapis.com/${local.bucket_name}/${local.webapp_deployment_bundle_filename}"
+  webapp_deployment_bundle_url      = "https://storage.googleapis.com/${local.bucket_name}/${local.webapp_deployment_bundle_filename}"
 }
--- modules/webapp/scripts/bucket_webapp_bundle_provisioner.sh
+++ modules/webapp/scripts/bucket_webapp_bundle_provisioner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[START] --- Open Targets Platform Web Application Provisioner (Bundle Version) ---"
-echo "[PWD] Current directory `pwd`"
+echo "[PWD] Current directory $(pwd)"
 mkdir -p ${working_dir}
 cd ${working_dir}
 echo "[WORKDIR] Working dir at '${working_dir}', cleaning possible previous runs"
@@ -12,10 +12,10 @@ echo "[BUILD] Download bundle from '${url_bundle_download}'"
 wget --no-check-certificate "${url_bundle_download}"
 cd ${path_build}
 echo "[BUILD] Unpack bundle"
-tar xzvf "${working_dir}/bundle.tgz"
+tar xzvf "${working_dir}/bundle-genetics.tgz"
 echo "[BUILD] Attach deployment context at '${file_name_devops_context_instance}'"
 cp ${file_name_devops_context_template} ${file_name_devops_context_instance}
-for envvar in $( cat ${file_name_devops_context_instance} | egrep -o "DEVOPS[_A-Z]+$" ); do
+for envvar in $(cat ${file_name_devops_context_instance} | egrep -o "DEVOPS[_A-Z]+$"); do
     export key=${envvar}
     export value=${!envvar:-undefined}
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -37,5 +37,5 @@ echo "[PACKAGING] Preparing Web Application deployment bundle"
 tar czvf ../${deployment_bundle_filename} *
 cp ../${deployment_bundle_filename} .
 echo "[DEPLOY] Uploading webapp to bucket '${bucket_webapp_url}'"
-gsutil cp -r `pwd`/* ${bucket_webapp_url}
+gsutil cp -r $(pwd)/* ${bucket_webapp_url}
 echo "[DONE] Process Completed ---"
